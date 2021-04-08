02326565a0716b7aa1943634d418d5756f362740
albhasan <albhasan@gmail.com>
2021-04-08 23:04:49 +0000

    Train model using SOM processed samples and merging some labels.

--- scripts/07_train_model.R
+++ scripts/07_train_model.R
@@ -5,11 +5,12 @@ library(purrr)
 library(sits)
 
 
+
 #--- Configuration ----
 
-samples_dir <- "/home/alber.ipia/Documents/bdc_pantanal/data/samples"
-model_file  <- "/home/alber.ipia/Documents/bdc_pantanal/results/second_classification/ml_model.rds"
-stopifnot(dir.exists(samples_dir))
+samples_file <- "/home/alber.ipia/Documents/bdc_pantanal/data/samples/som/samples_pantanal_som.rds"
+model_file  <- "/home/alber.ipia/Documents/bdc_pantanal/results/third_classification/ml_model.rds"
+stopifnot(file.exists(samples_file))
 stopifnot(dir.exists(dirname(model_file)))
 
 ml_method <- sits::sits_rfor(trees = 2000)
@@ -20,48 +21,32 @@ source("./scripts/00_util.R")
 
 #---- Script ----
 
-samples_tb <- samples_dir %>%
-    list.files(pattern = "*.rds",
-               full.names = TRUE,
-               recursive = TRUE) %>%
-    tibble::as_tibble() %>%
-    dplyr::rename(file_path = value) %>%
-    dplyr::mutate(file_name = file_path %>%
-                      tools::file_path_sans_ext() %>%
-                      basename()) %>%
-    tidyr::separate(file_name,
-                    into = c("source", "tile", "start_date", "end_date",
-                             "batch" ),
-                    sep = "_") %>%
-    dplyr::mutate(start_date = lubridate::as_date(start_date),
-                  end_date   = lubridate::as_date(end_date)) %>%
-    dplyr::filter(start_date >= lubridate::as_date("2018-01-01"),
-                  end_date   <= lubridate::as_date("2018-12-31")) %>%
-    ensurer::ensure_that(nrow(.) > 0,
-                         err_desc = "No sample files were foound!") %>%
-    dplyr::mutate(data = purrr::map(file_path, readRDS)) %>%
-    dplyr::pull(data) %>%
-    dplyr::bind_rows() %>%
-    is_sits_valid() %>%
-    dplyr::mutate(cube = "pantanal")
-
+samples_tb <- samples_file %>%
+    readRDS() %>%
+    #dplyr::filter(eval == "clean") %>%
+    dplyr::select(-id, -old_label, -id_sample, -id_neuron, -eval, -post_prob) %>%
+    dplyr::mutate(label = dplyr::recode(label,
+                                             "FORMAÇÃO SAVÃNICA"  = "F. CAMPESTRE SAVÃNICA",
+                                             "FORMAÇÃO CAMPESTRE" = "F. CAMPESTRE SAVÃNICA")) %>%
+    is_sits_valid()
 samples_tb %>%
     dplyr::count(label)
+# NEW
 # label                              n
-# * <chr>                          <int>
-# 1 AFLORAMENTO ROCHOSO                1
-# 2 CAMPO ALAGADO E ÁREA PANTANOSA   213
-# 3 CANA                               1
-# 4 FLORESTA PLANTADA                  8
-# 5 FORMAÇÃO CAMPESTRE               335
-# 6 FORMAÇÃO FLORESTAL               365
-# 7 FORMAÇÃO SAVÃNICA                221
-# 8 INFRAESTRUTURA URBANA              5
-# 9 LAVOURA PERENE                     2
-# 10 LAVOURA TEMPORÁRIA                56
-# 11 OUTRA ÁREA NÃO VEGETADA            5
-# 12 PASTAGEM                         489
-# 13 RIO, LAGO E OCEANO                33
+# 1 AGRICULTURA                      695
+# 2 CAMPO ALAGADO E ÁREA PANTANOSA   209
+# 3 F. CAMPESTRE SAVÃNICA           1181
+# 4 FORMAÇÃO FLORESTAL              1586
+# 5 PASTAGEM                        2930
+#
+# ORIGINAL
+# label                              n
+# 1 AGRICULTURA                      695
+# 2 CAMPO ALAGADO E ÁREA PANTANOSA   209
+# 3 FORMAÇÃO CAMPESTRE               538
+# 4 FORMAÇÃO FLORESTAL              1586
+# 5 FORMAÇÃO SAVÃNICA                643
+# 6 PASTAGEM                        2930
 
 ml_model <- sits::sits_train(samples_tb,
                              ml_method = ml_method)
