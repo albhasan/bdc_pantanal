5efa3dad8eb1834cebc6d97da2873f20799755ab
albhasan <albhasan@gmail.com>
2021-04-08 23:03:18 +0000

    Added deprecation notice.

--- scripts/01_download_cubes.R
+++ scripts/01_download_cubes.R
@@ -5,7 +5,7 @@ library(readr)
 library(sits)
 library(tidyr)
 
-
+stop("Deprecated. Use BDC image services intead!")
 
 #---- Setup ----
 
--- scripts/04_eda.R
+++ scripts/04_eda.R
@@ -1,18 +1,51 @@
 library(dplyr)
+library(ensurer)
 library(ggplot2)
 library(purrr)
+library(sf)
 library(stringr)
 library(tidyr)
 
-samples_tb <- paste0("./data/samples/2018") %>%
-    list.files(pattern = "*.rds",
-               full.names = TRUE) %>%
-    tibble::enframe() %>%
-    dplyr::rename(file_path = value) %>%
-    dplyr::mutate(rds = purrr::map(file_path, readRDS)) %>%
-    dplyr::select(rds) %>%
-    tidyr::unnest(rds) %>%
+
+source("./scripts/00_util.R")
+
+
+samples_file <- "./data/samples/samples_2018.rds"
+bdc_grid_shp <- "./data/bdc_grid/BDC_MD.shp"
+stopifnot(file.exists(samples_file))
+stopifnot(file.exists(bdc_grid_shp))
+
+bdc_grid <- bdc_grid_shp %>%
+    sf::read_sf() %>%
+    sf::st_transform(crs = 4326) %>%
+    dplyr::select(id)
+
+sits_samples_tb <- samples_file %>%
+    readRDS() %>%
+    sf::st_as_sf(coords = c("longitude", "latitude"),
+                 crs = 4326,
+                 remove = FALSE) %>%
+    sf::st_join(bdc_grid) %>%
+    sf::st_set_geometry(NULL) %>%
+    dplyr::filter(id %in% c("040049", "040050", "040051", "040052", "040053",
+                            "041049", "041050", "041051", "041052", "041053",
+                            "042050", "042051", "042052")) %>%
+    dplyr::select(-id) %>%
+    (function(x){
+        x %>%
+            dplyr::group_by(label) %>%
+            dplyr::summarise(Samples = n()) %>%
+            print(n = Inf)
+        return(x)
+    }) %>%
     mutate(cube = "pantanal") %>%
+    ensurer::ensure_that(nrow(.) > 0,
+                         err_des = "No samples match!")
+
+sits_samples_tb %>%
+    is_sits_valid() %>%
+
+samples_tb <- sits_samples_tb %>%
     tidyr::unnest(time_series) %>%
     dplyr::select(-longitude, -latitude, -cube,
                   Label = label) %>%
@@ -29,6 +62,8 @@ my_date <- samples_tb %>%
 
 
 #---- Plot samples' time series ----
+
+
 
 f_plot <- function(x){
     x %>%
@@ -86,70 +121,5 @@ plot_tb %>%
                     height = 420,
                     units = "mm")
 
-
-
-#---- PCA ----
-
-# Return the standard deviation explained by each principal component.
-pca_sd <- function(x){
-    x %>%
-        prcomp(center = TRUE, scale = TRUE) %>%
-        magrittr::extract2("sdev") %>%
-        magrittr::set_names(stringr::str_c("PC",
-                                           str_pad(1:length(.), pad = "0",
-                                                   width = 2))) %>%
-        dplyr::bind_rows() %>%
-        return()
-}
-
-plot_pca <- samples_tb %>%
-    dplyr::select(-start_date, -end_date, -Index) %>%
-    dplyr::group_by(Label) %>%
-    tidyr::nest() %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(data_bands = purrr::map(data, dplyr::select, B1:B7),
-                  data_indices = purrr::map(data, dplyr::select, EVI:NDVI)) %>%
-    dplyr::mutate(pca_bands = purrr::map(data_bands, pca_sd),
-                  pca_indices = purrr::map(data_indices, pca_sd))
-
-plot_pca %>%
-    tidyr::unnest(pca_bands) %>%
-    dplyr::select(-data, -data_bands, -data_indices, -pca_indices) %>%
-    tidyr::pivot_longer(cols = tidyselect::starts_with("PC"),
-                        names_to = "PC", values_to = "SD") %>%
-    dplyr::group_by(Label) %>%
-    dplyr::mutate(cum_SD = cumsum(SD),
-                  total = sum(SD),
-                  cum_norm_SD = cum_SD/total) %>%
-    dplyr::select(-total) %>%
-    dplyr::ungroup() %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_step(ggplot2::aes(x = PC, y = cum_norm_SD,
-                                    group = Label, color = Label)) +
-    ggplot2::geom_hline(yintercept = 0.95, linetype = "dashed") +
-    ggplot2::ggtitle("PCA using bands") +
-    ggplot2::ylab("Explained SD") +
-    ggplot2::ylim(0, 1) +
-    ggplot2::ggsave(filename = paste0("./data/samples/plot_samples_pca_bands_",
-                                      my_date, ".png"))
-
-plot_pca %>%
-    tidyr::unnest(pca_indices) %>%
-    dplyr::select(-data, -data_bands, -data_indices, -pca_bands) %>%
-    tidyr::pivot_longer(cols = tidyselect::starts_with("PC"),
-                        names_to = "PC", values_to = "SD") %>%
-    dplyr::group_by(Label) %>%
-    dplyr::mutate(cum_SD = cumsum(SD),
-                  total = sum(SD),
-                  cum_norm_SD = cum_SD/total) %>%
-    dplyr::select(-total) %>%
-    dplyr::ungroup() %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_step(ggplot2::aes(x = PC, y = cum_norm_SD,
-                                    group = Label, color = Label)) +
-    ggplot2::geom_hline(yintercept = 0.95, linetype = "dashed") +
-    ggplot2::ggtitle("PCA using indices") +
-    ggplot2::ylab("Explained SD") +
-    ggplot2::ylim(0, 1) +
-    ggplot2::ggsave(filename = paste0("./data/samples/plot_samples_pca_indices_",
-                                      my_date, ".png"))
+sits_samples_tb %>%
+    saveRDS(file = "./data/samples/samples_pantanal.rds")
